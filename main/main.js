a5376e30d5e8e9871bd8436a9d0ada70683e499e
451 <xuan199511@qq.com>
2019-05-31 23:50:31 +0800

    refactor

--- main/main.js
+++ main/main.js
@@ -2,40 +2,32 @@ module.exports = function main(inputs) {
 	let distance = inputs.distance;
 	let parkTime = inputs.parkTime;
 	
-	price = 0 ;
-	basic_price = 6 ; //起步价
-	parkTime_price = 0.25 ; //等待时间计费
-	distance_price = 0.8 ; //普通计费
-	surpass_price = distance_price * (1 + 0.5);  //超出增收
-	basic_distance = 2 ; //起步路程
-	surpass_distance = 8 ; //超出路程
+	INIT_PRICE = 6 ;
+	PARK_TIME_UNIT_PRICE = 0.25 ; 
+	DISTANCE_UNIT_PRICE = 0.8 ;
+	INIT_DISTANCE = 2 ;
+	PRICE_RAISED_RATE = 0.5	
+	NORMAL_DISTANCE = 8 ;
 	
-  
+	const parkPrice = PARK_TIME_UNIT_PRICE * parkTime;
+	const finalPrice = INIT_PRICE + calculatePriceBeyondInitDistance(distance) + parkPrice;
 	
-	if(distance <= basic_distance ){
-		short_distance(parkTime);
-	}
-	
-	if(distance > basic_distance && distance <= surpass_distance){
-		middle_distance(distance,parkTime);
-	}
-	
-	if(distance > surpass_distance ){
-		long_distance(distance,parkTime);
-	}
-	
-    return Math.round(price);
-	
+	return Math.round(finalPrice);
 };
-	function short_distance(parkTime){
-		price = basic_price + parkTime_price * parkTime;
-		return price;
-	}
-	
-	function middle_distance(distance,parkTime){
-		price = short_distance(parkTime) + (distance-basic_distance) * distance_price;
-	}
-	
-	function long_distance(distance,parkTime){
-		price = short_distance(parkTime) + (surpass_distance - basic_distance)* distance_price + (distance-surpass_distance) * surpass_price;
-	}
+
+function calculatePriceBeyondInitDistance(distance) {
+	if(distance <= INIT_DISTANCE)
+		return 0;
+	if(distance <= NORMAL_DISTANCE) {
+	 return (distance - INIT_DISTANCE) * DISTANCE_UNIT_PRICE;
+    }
+	return calculatePriceBeyondNormalDistance(distance);	
+}
+
+function calculatePriceBeyondNormalDistance(distance) {
+	const unitPriceBeyondNormalDistance = DISTANCE_UNIT_PRICE + DISTANCE_UNIT_PRICE * PRICE_RAISED_RATE; 
+    return (NORMAL_DISTANCE - INIT_DISTANCE) * DISTANCE_UNIT_PRICE + (distance - NORMAL_DISTANCE) * unitPriceBeyondNormalDistance;				
+}
+
+
+
